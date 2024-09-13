770abfe9dfc22f4aecdaa0d3cc86440b150468ff
Visibyte <nathanglass26@gmail.com>
2024-09-13 17:27:55 +0100

    add pot total back to users balance on deletion

--- src/app/(protected)/pots/actions/delete-pot-action.ts
+++ src/app/(protected)/pots/actions/delete-pot-action.ts
@@ -5,7 +5,6 @@ import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import type { PotActionResult } from "../types";
 
-
 export async function deletePotAction(potId: string): Promise<PotActionResult> {
   const { user } = await validateServerSession();
 
@@ -13,19 +12,47 @@ export async function deletePotAction(potId: string): Promise<PotActionResult> {
     return { error: "User must be authenticated to delete a pot." };
   }
 
-  const result = await prisma.pot.deleteMany({
+  const pot = await prisma.pot.findUnique({
     where: {
       id: potId,
-      userId: user.id,
+    },
+    select: {
+      total: true,
+      userId: true,
     },
   });
 
-  if (result.count === 0) {
+  if (!pot || pot.userId !== user.id) {
     return {
       error: "No pot found or you're not authorised to delete this pot.",
     };
   }
 
+  const usersBalance = await prisma.user.findUnique({
+    where: { id: user.id },
+    select: { currentBalance: true },
+  });
+
+  if (!usersBalance) {
+    return { error: "User does not have a balance." };
+  }
+
+  const updatedBalance = usersBalance.currentBalance + pot.total;
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { currentBalance: updatedBalance },
+  });
+
+  const result = await prisma.pot.delete({
+    where: { id: potId },
+  });
+
+  if (!result) {
+    return { error: "An error occurred while trying to delete the pot." };
+  }
+
+  revalidatePath("/");
   revalidatePath("/pots");
 
   return { success: true };
